0e44b91f7ab85b6aa0cec7804158f64982758ea2
Oleksandr Moroz <ostapenkomoroz1@gmail.com>
2018-08-08 22:15:57 +0300

    second commit

--- Js12/1.js
+++ Js12/1.js
@@ -1,11 +1,20 @@
-document.getElementById('slider-left').onclick = sliderLeft;
+//document.getElementById('slider-left').onclick = sliderLeft;
+autoSlider();//виклик виконання функції autoSlider, місце виклику не важливе (можна вверху, можна внизу)
 var left = 0;
+var timer;//для автослайдера
 
-function sliderLeft() {
-    var polosa = document.getElementById('polosa');
-    left = left - 128;
-    if (left < -512) {
-        left = 0;
-    }
-    polosa.style.left = left + 'px';//двигаєм полосу вліво на 128 px
+//Зробимо слайдер автоматичним за допомогою settimeout i setinterval
+
+function autoSlider() {
+    timer = setTimeout(function() {
+        var polosa = document.getElementById('polosa');
+        left = left - 128;
+        if (left < -512) {
+            left = 0;
+            clearTimeout(timer);//обнулення таймера
+        }
+        polosa.style.left = left + 'px';//двигаєм полосу вліво на 128 px
+        autoSlider();//виклик виконання функції autoSlider для зациклювання прокрутки слайдера
+    }, 1400);//тут 1000 рівна 1 секунді, прокрутить слайдер на 1 картинку за 1 сек.
 }
+
